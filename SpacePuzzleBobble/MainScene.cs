8c51bbc40f43dbf8a507ab6bca09a8df158fad1e
Kaewphet Nuruang <66639008+nobiofla@users.noreply.github.com>
2023-03-01 19:41:38 +0700

    Update MainScene

--- SpacePuzzleBobble/MainScene.cs
+++ SpacePuzzleBobble/MainScene.cs
@@ -4,12 +4,12 @@ using Microsoft.Xna.Framework.Input;
 
 namespace SpacePuzzleBobble
 {
-    public class Game1 : Game
+    public class MainScene : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-
-        public Game1()
+        
+        public MainScene()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
--- SpacePuzzleBobble/Program.cs
+++ SpacePuzzleBobble/Program.cs
@@ -1,3 +1,3 @@
 ﻿
-using var game = new SpacePuzzleBobble.Game1();
+using var game = new SpacePuzzleBobble.MainScene();
 game.Run();
